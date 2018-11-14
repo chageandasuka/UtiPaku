a2e075dc1dbd29eccf19faf5bd53f0d56195386a
chageandasuka <234kgd@ahk.jp>
2018-11-15 00:20:43 +0900

    ・コード内いじらなくてもURL入れればアドレス操作で3会場取得するようにした。 ・レース毎のヘッダ出力を廃止。 ・配列定義を{}にしていたら、実行するたびに要素順序が変わってしまっていたので[]に変更。 　#自分だけ？

--- Strider/Get_Stride_DataSheet.py
+++ Strider/Get_Stride_DataSheet.py
@@ -5,14 +5,16 @@ from u_def import table2csv as tbl2csv
 
 import pandas as pd
 from bs4 import BeautifulSoup
-
-
-repstr = '.html?idpm=ここに数字の列をいれとく'
+import re
 
 
 # HTML データを取得する
-url = input('>> ')
-u_tmp = url.replace(repstr, '')
+inurl = input('データシートURL >> ')
+
+# 後々replaceで使ったりする。
+idpm = re.findall('\.html.+', inurl)
+
+u_tmp = inurl.replace(idpm[0], '')
 ftmp = u_tmp[len(u_tmp)-9:len(u_tmp)-1]
 
 csvfile = ftmp + 'EWL_data.csv'
@@ -20,16 +22,22 @@ print(csvfile)
 
 # 東西＋ローカル
 # 2会場の時はどうしよう？また今度考える
-tablst = ['E', 'W', 'L']
-
+tablst = ['W', 'E', 'L']
+#print(tablst)
 for kaijou in tablst:
-    url = u_tmp[0:len(u_tmp)-1] + kaijou + repstr
+    url = u_tmp[0:len(u_tmp)-1] + kaijou + idpm[0]
     print(url)
 
     #csvfile = '00_test.csv'
     res = requests.get(url)
     res.encoding = res.apparent_encoding
     content = res.text
+
+    htstcd = res.status_code
+    # HTTPステータスコード 404
+    if  htstcd == 404:
+        print(str(htstcd) + ':' + kaijou)
+        continue
 
     soup = BeautifulSoup(content, "lxml")
     # 全ての<div>...</div>を取得
@@ -71,7 +79,22 @@ for kaijou in tablst:
         df2['ＳＴ指数順位'] = df2["ＳＴ指数"].rank(ascending=False, method='min')
         df2['仕上指数順位'] = df2["仕上指数"].rank(ascending=False, method='min')
 
+        # 区切りとしてレース毎にヘッダ出力してたけど不要な気もするので、Output_Stride_n_Labと同じ出力方式にしてみる
+        if hoge != 0:
+            #print("ヘッダーなし")
+            #df2.to_csv(csvfile, encoding="shift_jis", header=False, index=False, mode="a")
+            df2.to_csv(csvfile, encoding="shift_jis", header=False, index=False, mode="a")
+            # CSV ファイル (employee.csv) として出力（追記モード）
+        else:
+            #print("ヘッダーあり")
+            # hoge == 0の時だけヘッダー出力
+            #df2.to_csv(csvfile, encoding="shift_jis", index=False, mode="a")
+            df2.to_csv(csvfile, encoding="shift_jis", index=False, mode="a")
+
+        # CSV ファイル (employee.csv) として出力（追記モード）
+        #df2.to_csv(csvfile, encoding="shift_jis", index=False, mode="a")
+
         # レース情報のインデックスは0,3,6...なので
         hoge += 3
-        # CSV ファイル (employee.csv) として出力（追記モード）
-        df2.to_csv(csvfile, encoding="shift_jis", index=False, mode="a")
+
+input('終了するには Enter Key を押してください...')
