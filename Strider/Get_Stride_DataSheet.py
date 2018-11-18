39e05e71534135c6f789b7b039350d14c1328ef8
chageandasuka <234kgd@ahk.jp>
2018-11-18 21:49:58 +0900

    変更内容： 　東西ローカルの配列順序入れ替え 　レース情報列の分割

--- Strider/Get_Stride_DataSheet.py
+++ Strider/Get_Stride_DataSheet.py
@@ -20,7 +20,7 @@ csvfile = ftmp + 'EWL_data.csv'
 print(csvfile)
 
 # 東西＋ローカル
-tablst = ['W', 'E', 'L']
+tablst = ['E', 'W', 'L']
 #print(tablst)
 for kaijou in tablst:
     url = u_tmp[0:len(u_tmp)-1] + kaijou + idpm[0]
@@ -39,7 +39,7 @@ for kaijou in tablst:
 
     soup = BeautifulSoup(content, "lxml")
     # 全ての<div>...</div>を取得
-    course = soup.find_all("div", attrs={"class", "headding03"})
+    race_str = soup.find_all("div", attrs={"class", "headding03"})
 
     # table 要素を取得する
     # HTML 中のすべての table 要素が表として使われているとも
@@ -59,13 +59,58 @@ for kaijou in tablst:
         df2 = pd.DataFrame(rows[1:], columns=rows[0])
         #print(course[hoge].text)
         # レース情報をDataFrameに追加
-        df2['レース情報'] = course[hoge].text
+        #df2['レース情報'] = course[hoge].text
 
         # DataFrame末尾に追加したレース情報を先頭に移動する
-        cols = list(df2.columns.values)
-        cols = ['レース情報']  + [col for col in df2 if col != 'レース情報']
-        df2 = df2[cols]
+        #cols = list(df2.columns.values)
+        #cols = ['レース情報']  + [col for col in df2 if col != 'レース情報']
+        #df2 = df2[cols]
 
+        ##ここから　コース情報分割処理
+        # レース情報に含まれる全角スペースを基準にして分割する
+        #   0              1                  2            3
+        #['中山1R', '３歳未勝利３歳(馬齢)', 'ダ1200ｍ', '10:10発走']
+        # ひとまず右記の形式で出力する。　例：中山, 1R, ダ, 1200
+        # 0は会場名とレース番号を分ける
+        # 2はコースと距離を分ける
+        race_tmp = re.split('　', race_str[hoge].text)
+        print(race_tmp)
+
+        # 東京と1R分割
+        tmpstr = race_tmp[0]
+        kaijou = tmpstr[:2]
+        race_num = tmpstr[2:]
+
+        tmpstr = race_tmp[2]
+        course = tmpstr[:1]
+        distance = tmpstr[1:-1]
+        ##########################################
+        #distance = re.findall('[0-9]{4}', tmpstr)
+        #distance = distance[0]
+
+        df2['日付'] = ftmp
+
+        df2['会場'] = kaijou
+        df2['レース'] = race_num
+        df2['コース'] = course
+        df2['距離'] = distance
+
+        cols = list(df2.columns.values)
+        cols = ['距離']+ [col for col in df2 if col != '距離']
+        df2 = df2[cols]
+        cols = list(df2.columns.values)
+        cols = ['コース']+ [col for col in df2 if col != 'コース']
+        df2 = df2[cols]
+        cols = list(df2.columns.values)
+        cols = ['レース']+ [col for col in df2 if col != 'レース']
+        df2 = df2[cols]
+        cols = list(df2.columns.values)
+        cols = ['会場']+ [col for col in df2 if col != '会場']
+        df2 = df2[cols]
+        cols = list(df2.columns.values)
+        cols = ['日付']+ [col for col in df2 if col != '日付']
+        df2 = df2[cols]
+        ##ここまで　コース情報分割処理
         # for Debug
         # print(df2)
         df2[''] = ''
