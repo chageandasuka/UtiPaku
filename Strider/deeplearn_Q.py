06e76e93a1aebc3690e7d1426ef1ac34de3560be
UtiPakuPaku <44972784+UtiPakuPaku@users.noreply.github.com>
2018-11-20 01:46:40 +0900

    騎手名を勝率値へ変換するロジックを実装。 ただし、上記ファクターを使用すると単回収が著しく低下することを確認済み。 上記よりデフォルトではコメントアウトとする。 現状のファクター設定が手探りで調べた中では一番良い結果が出ている。 → 予想結果1位かつ単オッズ3倍以上10倍未満なら、単回収率97%(11/17～18にて確認)

--- Strider/deeplearn_Q.py
+++ Strider/deeplearn_Q.py
@@ -1,9 +1,10 @@
 import requests
 from u_def import get_tables as gtbl
 from u_def import parse_table as ptbl
-from u_def import table2csv as tbl2csv
+from u_def import JockeyTable as jtbl
 import pandas as pd
 from bs4 import BeautifulSoup
+
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.cluster import KMeans
 from sklearn import linear_model
@@ -19,8 +20,19 @@ df = pd.read_csv(fname, encoding="shift_jis")
 #print(df.head())
 
 print(df.info())
-df = df[["基準オッズ", "基準人気順位", "展開順位", "先行力", "追走力", "持久力", "持続力",
- "瞬発力", "ＳＴ指数", "仕上指数", "着", "人", "単勝"]]
+df = df[[
+            #"騎手名",
+            "展開順位",
+            "展開ゴール差",
+            "瞬発力",
+            "瞬発力順位",
+            "仕上指数",
+            "ＳＴ指数順位",
+            "仕上指数順位",
+            "着",
+            "人",
+            "単勝"
+       ]]
 #print(df.head())
 
 # 値の無い行、中止の行を削除
@@ -35,7 +47,6 @@ df = df[df["着"] != "除外"]
 ####df["単勝"] = df["単勝"].astype("float64") * 10
 #print(df["単勝"])
 ####df = df.astype('int16')
-df = df.astype('float64')
 
 #標準化テスト
 sc = StandardScaler()
@@ -46,7 +57,17 @@ sc = StandardScaler()
 #print(df.info())
 cols = df.columns.tolist()
 #print(cols)
+
+# 不要列削除
 x_dat = df.drop(columns=["着", "人", "単勝"])
+
+# 騎手名 → 数値変換
+x_dat = jtbl.JockeyTable(x_dat)
+print(x_dat.head())
+
+# float64へキャスト
+x_dat = x_dat.astype('float64')
+
 x_dat = pd.DataFrame(sc.fit_transform(x_dat))
 y_dat = df["着"]
 
@@ -62,12 +83,22 @@ for lp in range(1, 13):
     print(lp)
     print(df.head())
     # テストデータには結果列無いよ
-    df_t = df[["基準オッズ", "基準人気順位", "展開順位", "先行力", "追走力", "持久力", "持続力",
-     "瞬発力", "ＳＴ指数", "仕上指数"]]
+    df_t = df[[
+                #"騎手名",
+                "展開順位",
+                "展開ゴール差",
+                "瞬発力",
+                "瞬発力順位",
+                "仕上指数",
+                "ＳＴ指数順位",
+                "仕上指数順位"
+             ]]
+
+    # 騎手名 → 数値変換
+    test_X = jtbl.JockeyTable(df_t)
 
     #####df["基準オッズ"] = df["基準オッズ"] * 10
-    #test_X = df.astype('int16')
-    test_X = df_t.astype('float64')
+    test_X = test_X.astype('float64')
     test_X = pd.DataFrame(sc.fit_transform(test_X))
     print(test_X.head())
 
