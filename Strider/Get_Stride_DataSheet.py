18da5e52cf1f26e6d1d48e7aae1b206e06253029
chageandasuka <234kgd@ahk.jp>
2018-11-30 20:08:43 +0900

    URL入れた後に直前展開取るかどうかを聞かれます。 0か1で回答を求められ、1を回答すると取ります。それ以外は取りません。残念でした。

--- Strider/Get_Stride_DataSheet.py
+++ Strider/Get_Stride_DataSheet.py
@@ -1,14 +1,25 @@
 import requests
-from u_def import get_tables as gtbl
-from u_def import parse_table as ptbl
-
 import pandas as pd
 from bs4 import BeautifulSoup
 import re
 
+from u_def import get_tables as gtbl
+from u_def import parse_table as ptbl
+from u_def import get_tyokuten as getyoku
 
+# 最初の書き込みのみヘッダを出すためのフラグ
+h_flg = 0
 # HTML データを取得する
 inurl = input('データシートURL >> ')
+
+# データシートのあるアドレスの固定文字列
+CNST_URL = "https://stride.get-luck.jp/member/"
+
+SELECT_TYOKUTEN = input("直前展開予想取得する？[0 or 1] >> ")
+if SELECT_TYOKUTEN == '1':
+    print("直前ファクター取得する。")
+else:
+    print("直前ファクター取得しない。")
 
 # 後々replaceで使ったりする。
 idpm = re.findall('\.html.+', inurl)
@@ -40,7 +51,20 @@ for kaijou in tablst:
     soup = BeautifulSoup(content, "lxml")
     # 全ての<div>...</div>を取得
     race_str = soup.find_all("div", attrs={"class", "headding03"})
-
+# 直展リンク作成
+    tyokuten_url = []
+    # データシートページのリンクを取得
+    if SELECT_TYOKUTEN == '1':
+        elems = soup.select('a')
+        for elem in elems:
+            if elem.getText() == "直前":
+                tmp = elem.get('href')
+                tmp = tmp.replace('../', '')
+                tyokuten_url.append(CNST_URL + tmp)
+            else:
+                pass
+    else:
+        pass
     # table 要素を取得する
     # HTML 中のすべての table 要素が表として使われているとも
     # 限らないし、またそれが望んでいるものであるとも限らないので
@@ -52,6 +76,7 @@ for kaijou in tablst:
     #for num in tables:
     #    print(num)
     hoge = 0
+    tyoku_idx = 0
     for dmy in tables:
         rows = ptbl.parse_table(dmy)
         if not rows[0]:
@@ -83,10 +108,10 @@ for kaijou in tablst:
 
         tmpstr = race_tmp[2]
         course = tmpstr[:1]
-        distance = tmpstr[1:-1]
+        #distance = tmpstr[1:-1]
         ##########################################
-        #distance = re.findall('[0-9]{4}', tmpstr)
-        #distance = distance[0]
+        distance = re.findall('[0-9]{4}', tmpstr)
+        distance = distance[0]
 
         df2['日付'] = ftmp
 
@@ -121,9 +146,30 @@ for kaijou in tablst:
         df2['瞬発力順位'] = df2["瞬発力"].rank(ascending=False, method='min')
         df2['ＳＴ指数順位'] = df2["ＳＴ指数"].rank(ascending=False, method='min')
         df2['仕上指数順位'] = df2["仕上指数"].rank(ascending=False, method='min')
+################################################################################
+# 直前ファクター取得
+        if SELECT_TYOKUTEN == '1':
+        #print("直展:{}".format(tyokuten_url[tyoku_idx]))
+        #print("前{}".format(df2.shape))
+        #print(df2.head())
+            t_df = getyoku.get_tyokuten(tyokuten_url[tyoku_idx])
 
+            if len(t_df) == 0:
+                hoge += 3
+                #ridx += 1
+                tyoku_idx += 1
+                continue
+
+            # データシートと重複する列を除外する
+            t_df = t_df.drop(["馬番", "馬名", "騎手名", "ST指数", "仕上指数"], axis=1)
+            # データシートのDFと直前展開のDFを結合
+            df2 = pd.merge(df2, t_df, right_index=True, left_index=True)
+        else:
+            pass
+
+################################################################################
         # 区切りとしてレース毎にヘッダ出力してたけど不要な気もするので、Output_Stride_n_Labと同じ出力方式にしてみる
-        if hoge != 0:
+        if h_flg == 1:
             #print("ヘッダーなし")
             #df2.to_csv(csvfile, encoding="shift_jis", header=False, index=False, mode="a")
             df2.to_csv(csvfile, encoding="shift_jis", header=False, index=False, mode="a")
@@ -133,11 +179,13 @@ for kaijou in tablst:
             # hoge == 0の時だけヘッダー出力
             #df2.to_csv(csvfile, encoding="shift_jis", index=False, mode="a")
             df2.to_csv(csvfile, encoding="shift_jis", index=False, mode="a")
+            h_flg = 1
 
         # CSV ファイル (employee.csv) として出力（追記モード）
         #df2.to_csv(csvfile, encoding="shift_jis", index=False, mode="a")
 
         # レース情報のインデックスは0,3,6...なので
         hoge += 3
+        tyoku_idx += 1
 
 input('終了するには Enter Key を押してください...')
